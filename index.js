0541688e88e148f7dff7e1e26793d9849b357d55
Emily <emilyt62@gmail.com>
2022-12-15 21:01:57 +1100

    Basic Elements

---
+++ index.js
@@ -0,0 +1,26 @@
+// Display About Me section upon button click event
+function displayAbout() {
+    const about = document.getElementById("aboutReveal");
+    if (about.style.display === "none") {
+        about.style.display = "block";}
+    else {
+        about.style.display = "none";}
+    }
+
+// Display Projects section upon button click event
+function displayProjects() {
+    const projects = document.getElementById("projectsReveal");
+    if (projects.style.display === "none") {
+        projects.style.display = "block";}
+    else {
+        projects.style.display = "none";}
+    }
+
+// Display Connect With Me section upon button click event
+function displayConnect() {
+    const connect = document.getElementById("connectReveal");
+    if (connect.style.display === "none") {
+        connect.style.display = "block";}
+    else {
+        connect.style.display = "none";}
+    }
